4e86943478cef53eabb22b3d0d559a21bac48479
Leonardo Gross <leogrossms@gmail.com>
2023-04-17 11:10:28 -0300

    Push LS e deletar dados funcionando Erro:innerHTML não está funcionando (dados não estão sendo adicionandos)

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-//variavel transacoesCru recebe os itens dentro de 'transacoes', se após isso, não tiver nada dentro de  transacoesCru, 'transacoes' recebe transacoesCru em objeto JS, se tiver algo dentro de transacoesCru, transacoes recebe uma array vazia (para que se possa adicionar os itens da tabela)
-var transacoesCru = localStorage.getItem('transacoes')
-if (transacoesCru != null) {
-    var transacoes = JSON.parse(transacoesCru)
-} else {;   
-    var transacoes = [];
-}
 
-//mostra as transações já feitas
-function mostraTransacoes() {
-    transacoes= []
-}
+function desenha_tabela() {
+  //variavel transacoesCru recebe os itens dentro de 'transacoes', se após isso, não tiver nada dentro de  transacoesCru, 
+  //'transacoes' recebe transacoesCru em objeto JS, se tiver algo dentro de transacoesCru, transacoes recebe uma array vazia 
+  //(para que se possa adicionar os itens da tabela)
+  var transacoesCru = localStorage.getItem('transacoes')
+  if (transacoesCru != null) {
+      var transacoes = JSON.parse(transacoesCru)
+  } else {;   
+      var transacoes = [];
+  }
 
-
-//escreve no HTML em forma de tabela os itens da tabela em suas respectivas posições 
-for (item in transacoes) {
+  //limpa os itens ta tabela
+  document.querySelector('table.tabelaExtrato tbody').innerHTML = ''
+  //re-escreve no HTML em forma de tabela todos os itens da tabela em suas respectivas posições     
+  for (item in transacoes) {
     document.querySelector('table.tabelaExtrato tbody').innerHTML += (
     `<tr>
         <td>${(transacoes[item].compra == 'true' ? ' - ' : ' + ')}</td>
@@ -21,7 +21,9 @@ for (item in transacoes) {
         <td style='text-align:right'>R$&nbsp;${transacoes[item].valor}</td>
     </tr>`
     )
+  }
 }
+
 
 //quando clica pra submeter formulário, a função 'preventDefault()' previne a ação padrão do navegador que seria submeter o formulário 
 function testaFormulario(e) {
@@ -35,23 +37,77 @@ function testaFormulario(e) {
         var transacoes = [];
     }
 
-    //ele mostra as transacoes e manda as informações dos inputs para seus respectivos índices na array transacao, 
-    function mostraTransacoes() {
-        transacoes= []
-    }   
+  //ele mostra as transacoes e manda as informações dos inputs para seus respectivos índices na array transacao
+  function mostraTransacoes() {
+    transacoes= []
+    
+}  
 
-    transacoes.push({
-        compra: e.target.elements['tipo_transacao'].value,
-        name: e.target.elements['nome'].value,
-        valor: e.target.elements['valor'].value
-    })
+transacoes.push({
+  compra: e.target.elements['tipo_transacao'].value,
+  name: e.target.elements['nome'].value,
+  valor: e.target.elements['valor'].value
+})
 
-    //transforma os objetos JS em strings JSON e cadastra os itens nos seus respectivos índices
-    localStorage.setItem('transacoes', JSON.stringify(transacoes))
 
-    //recarrega a página
-    paginaInicial()
+
+//transforma os objetos JS em strings JSON e cadastra os itens nos seus respectivos índices
+localStorage.setItem('transacoes', JSON.stringify(transacoes))
 }
+
+//função que calcula e mostra o total do extrato até agora
+function totalExtrato() {
+  var total = 0;
+  let valorInput;
+  
+  for (item in transacoes) {
+    if (transacoes[item].tipo_transacao == "true") {
+      valorInput = transacoes[item];
+      total -= Number(transacoes[item].valor);
+    } else {
+        total += Number(transacoes[item].valor);
+      }
+    }
+  
+  if (transacoes.length > 0) {
+
+    document.querySelector("table.tabelaExtrato tfoot").innerHTML = `
+      <tr>
+        <td class="tabela_rodape">Total</td>
+        <td class="tabela_rodape">${formatter.format(total)}</td>
+      </tr>`
+
+      document.querySelector("table tfoot").innerHTML += `
+      <tr>
+        <td></td>
+        <td>${Math.sign(total) > 0 ? "[LUCRO]" : "[PREJUÍZO]"}</td>
+      </tr>`
+  }
+}
+
+
+
+//deleta itens do localStorage e consequentemente da tabela
+
+function deletaLocalStorage() {
+  var transacoes = []
+  if(transacoes.length <= 0) {
+      alert("Nenhum registro de transação");
+    } else {
+      let caixaTexto = confirm("Deseja excluir as transações?");
+      
+      if(caixaTexto == true) {
+        localStorage.clear();
+        alert("Transações excluídas");
+      } else {
+        alert("Exclusões canceladas");
+      }
+    }
+  }
+
+let linkExcluir = document.getElementById("link_limpar");
+linkExcluir.addEventListener("click", deletaLocalStorage)
+
 
 //Função para formatação monetária, nesse caso em moeda brasileira
 function formatterCurrency(value) {
@@ -68,63 +124,6 @@ var formatter = new Intl.NumberFormat("pt-BR", {
   currency: "BRL",
   maximumFractionDigits: 2,
 });
-
-//função que calcula e mostra o total do extrato até agora
-function totalExtrato() {
-    var total = 0;
-    let valorInput;
-  
-    for (item in transacoes) {
-      if (transacoes[item].tipo_transacao == "true") {
-        valorInput = transacoes[item];
-        total -= Number(transacoes[item].valor);
-      } else {
-        total += Number(transacoes[item].valor);
-      }
-    }
-  
-    if (transacoes.length > 0) {
-
-      document.querySelector("table.tabelaExtrato conteudo_tabela").innerHTML = `
-        <tr>
-          <td class="tabela_rodape">Total</td>
-          <td class="tabela_rodape">${formatter.format(total)}</td>
-        </tr>
-        `
-
-        document.querySelector("table tfoot").innerHTML += `
-        <tr>
-          <td>${Math.sign(total) > 0 ? "[LUCRO]" : "[PREJUÍZO]"}</td>
-        </tr>
-        `
-        //TENTANDO excluir a informação 'Nenhuma transação cadastrada'
-        // var conteudo_tabela = document.querySelector('conteudo_tabela'); 
-        // conteudo_tabela.remove('td')
-    }
-  }
-
-function deletaLocalStorage() {
-    if(transacoes.length <= 0) {
-        alert("Nenhum registro de transação");
-      } else {
-        let caixaTexto = confirm("Deseja excluir as transações?");
-      
-        if(caixaTexto == true) {
-          localStorage.clear();
-          alert("Transações excluídas");
-        } else {
-          alert("Exclusões canceladas");
-        }
-      }
-      paginaInicial()
-    }
-
-let linkExcluir = document.getElementById("link_limpar");
-linkExcluir.addEventListener("click", deletaLocalStorage)
-
-function paginaInicial() {
-    location.href="./index.html"
-}
 
 //Função máscara monetária para o padrão BR
 function MascaraMoeda(objTextBox, SeparadorMilesimo, SeparadorDecimal, e){  
@@ -167,3 +166,4 @@ function MascaraMoeda(objTextBox, SeparadorMilesimo, SeparadorDecimal, e){
   }  
   return false;  
 }
+
